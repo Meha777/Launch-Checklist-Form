b30a495f5e08a5e94fa97b09526d46191947d59e
Meha777 <mafework28@gmail.com>
2021-03-25 02:15:43 -0500

    Launch-Checklist-Form

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 // Write your JavaScript code here!
 
+
+   // window.addEventListener("load", function() {
+   //    let launchForm= document.getElementById(launchForm");
+   //    launchForm.addEventListener("submit", function(event) {
+
+      const formSubmit = document.getElementById("formSubmit")
+      formSubmit.addEventListener("submit",function(event){
+
+         
+         event.preventDefault()
+
+
+   //       let usernameInput = document.querySelector("input[name=username]");
+   //       let teamName = document.querySelector("input[name=team]");
+   //       if (usernameInput.value === "" || teamName.value === "") {
+   //          alert("All fields are required!");
+   //          // stop the form submission
+   //          event.preventDefault();
+   //       }
+   //    });
+   // });
+});
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
