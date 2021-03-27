7b6e88f73a7b690966e121a6a7f6a7035b4d9559
Meha777 <mafework28@gmail.com>
2021-03-27 13:31:42 -0500

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 
 
    window.addEventListener("load", function() {
+      const json = fetch("planets.json");
+      json.then(function(response){
+         response.json().then( function(jsonArray) {
+            console.log(jsonArray)
+      });
       const form = document.querySelector("form");
       form.addEventListener("submit",function(event) {
          event.preventDefault()
+
             
    
          let pilotName = document.querySelector("input[name=pilotName]");
@@ -28,71 +34,54 @@
         
 
          let pilotStatus = document.getElementById("pilotStatus");
-         pilotStatus.innerHTML = `Pilot Chris is ready for lanuch`
+         pilotStatus.innerHTML = `Pilot Chris is ready for lanuch`;
       
          let copilotStatus = document.getElementById("copilotStatus");
-         copiloteStatus.innerHTML = `Co-pilot Blake is ready for launch`
+         copilotStatus.innerHTML = `Co-pilot Blake is ready for launch`;
       
          let fuelStatus = document.getElementById("fuelStatus");
-         fuelStatus.innerHTML = `Fuel level high enough for launch`
+         fuelStatus.innerHTML = `Fuel level high enough for launch`;
 
          let cargoStatus = document.getElementById("cargoStatus");
-         cargoStatus.innerHTML = `Cargo mass low enough for launch`
+         cargoStatus.innerHTML = `Cargo mass low enough for launch`;
 
          let launchStatus = document.getElementById("launchStatus");
-         launchStatus.innerHTML = `Awaiting Information Before Launch`
+         launchStatus.innerHTML = `Awaiting Information Before Launch`;
 
-         let faultyStatus = document.getElementById("faultyItems");
+         let visibilityStatus = document.getElementById("faultyItems");
+
+         // let faultyStatus = document.getElementById("faultyItems");
 
         
 
       
          if(fuelLevel.value < 10000) {
-            event.preventDefault()
-            fuelStatus.innerHTML = `Fuel level is too low for launch`
-         
-
-            faultyStatus.innerHTML = `
-            <ol>
-                    <li>${pilotStatus.innerHTML}</li>
-                    <li>${copilotStatus.innerHTML}</li>
-                    <li>${fuelStatus.innerHTML}</li>
-                    <li>${cargoStatus.innerHTML}</li>
-          </ol>
-            `
+            visibilityStatus.style.visibility = "visible";
+            fuelStatus.innerHTML = `Fuel level is too low for launch`;
+           
             launchStatus.innerHTML = `Shuttle not ready for launch`;
             launchStatus.style.color = "red"
             
-         }    
-         
-
-         if (cargoMass.value > 10000) {
-            cargoStatus.innerHTML = `cargo mass is too large for launch`
-            faultyStatus.innerHTML = `
-           < ol>
-                    <li>${pilotStatus.innerHTML}</li>
-                    <li>${copiloteStatus.innerHTML}</li>
-                    <li>${fuelStatus.innerHTML}</li>
-                    <li>${cargoStatus.innerHTML}</li>
-          </ol>
-               `
+         }  else  if (cargoMass.value > 10000) {
+            cargoStatus.innerHTML = `cargo mass is too large for launch`;
                launchStatus.innerHTML = `Shuttle not ready for launch`;
                launchStatus.style.color = "red"
-         }
-
-         if (pilotStatus.innerHTML === `Pilot Chris is ready for lanuch` && 
-            copiloteStatus.innerHTML === `Co-pilot Blake is ready for launch` &&
-            fuelStatus.innerHTML === `Fuel level high enough for launch` &&
-            cargoStatus.innerHTML === `Cargo mass low enough for launch`){
-               launchStatus.innerHTML = "Shuttle is ready for launch"
-               launchStatus.style.color = "green"
-            }
+         }  else { 
+               launchStatus.innerHTML = "Shuttle is ready for launch";
+               launchStatus.style.color = "green";
+            } 
+           
+         
+         
+         });
+            
+         
 
 
    });
 });
 
-
+// https://handlers.education.launchcode.org/static/planets.json
 
 
 
